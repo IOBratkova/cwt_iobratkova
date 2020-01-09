495b41cee07a2256f609f9ee9e9acb274ddae0cd
Irina Bratkova <bratckova.i@yandex.ru>
2020-01-09 11:29:21 +0300

    update

--- project/program.py
+++ project/program.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
+
 from classes.page import Page
 
 driver = webdriver.Chrome('./chromedriver')
 url_google = 'http://www.google.com'
 url_yandex = 'https://ya.ru'
+
+driver.get(url_yandex)
+driver.find_element_by_id('text').send_keys('кот')
+kot = driver.find_element_by_id('text').get_attribute('value')
+
+print(kot)
 
 ya_page = Page(driver, url_yandex)
 
@@ -11,3 +18,4 @@ ya_page.open_url()
 
 ya_page.find_element_by_id('text')
 ya_page.paste_to_element('text', 'кот')
+text = ya_page.find_element_by_id('text')
