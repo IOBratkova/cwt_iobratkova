792167a9631dbb690d7eeb7aa264964b63b30e87
Irina Bratkova <bratckova.i@yandex.ru>
2020-01-09 12:26:37 +0300

    update

--- project/program.py
+++ project/program.py
@@ -1,21 +1,33 @@
 from selenium import webdriver
-
+from classes.signinpage import SignInPage
 from classes.page import Page
 
+# driver = webdriver.Chrome('./chromedriver')
+# url_google = 'http://www.google.com'
+# url_yandex = 'https://ya.ru'
+#
+# driver.get(url_yandex)
+# driver.find_element_by_id('text').send_keys('кот')
+# kot = driver.find_element_by_id('text').get_attribute('value')
+#
+# print(kot)
+#
+# ya_page = Page(driver, url_yandex)
+#
+# ya_page.open_url()
+#
+# ya_page.find_element_by_id('text')
+# ya_page.paste_to_element('text', 'кот')
+# text = ya_page.find_element_by_id('text')
+
+url = 'http://127.0.0.1:8080/#/signup'
 driver = webdriver.Chrome('./chromedriver')
-url_google = 'http://www.google.com'
-url_yandex = 'https://ya.ru'
+driver.maximize_window()
+signinpage = SignInPage(driver, url)
+signinpage.open_url()
 
-driver.get(url_yandex)
-driver.find_element_by_id('text').send_keys('кот')
-kot = driver.find_element_by_id('text').get_attribute('value')
-
-print(kot)
-
-ya_page = Page(driver, url_yandex)
-
-ya_page.open_url()
-
-ya_page.find_element_by_id('text')
-ya_page.paste_to_element('text', 'кот')
-text = ya_page.find_element_by_id('text')
+print(signinpage.master())
+signinpage.click_radio_button_master()
+print(signinpage.master())
+signinpage.select_value_to_master(2)
+print('+++++++++++++++++++++++++++++')
