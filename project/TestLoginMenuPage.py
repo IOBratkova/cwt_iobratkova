5f426aa27ecee2c830f4015812f52a932b597f3f
Irina Bratkova <bratckova.i@yandex.ru>
2020-01-10 15:25:25 +0300

    LoginMenu: успешный вход

--- project/TestLoginMenuPage.py
+++ project/TestLoginMenuPage.py
@@ -1,5 +1,5 @@
 import unittest
-from classes.page import Page
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from classes.loginmenu import LoginMenu
 
@@ -9,6 +9,11 @@ class TestLoginMenuPage(unittest.TestCase):
         self.url = 'http://127.0.0.1:8080/#/'
         self.driver = webdriver.Chrome('./chromedriver')
         self.lm = LoginMenu(self.driver, self.url)
+
+    def wait_update(self, oru_user):
+        WebDriverWait(self.lm.driver, 5).until(
+            lambda tmp: self.driver.find_element_by_id('user-name').text == oru_user
+        )
 
     def test_should_not_log_in(self):
         self.start_up()
@@ -35,3 +40,15 @@ class TestLoginMenuPage(unittest.TestCase):
         result = self.lm.click_login_button()
         wait_result = ([(True, '#username'), ('Проверьте пароль', '#password')], None)
         self.assertEqual(result, wait_result)
+
+    def test_should_be_log_in(self):
+        self.start_up()
+        self.lm.open_url()
+        self.lm.click_dropdown_sign_in()
+        self.lm.set_username('pwd')
+        self.lm.set_password('pwd')
+        our_user = 'Привет, pwd!' # запоминаем юзеа
+        result = self.lm.click_login_button() # кнопкожмяк
+        self.wait_update(our_user)
+        user = self.driver.find_element_by_id('user-name').text
+        self.assertEqual(user, our_user)
