0e28f3156751f2c0720ee3c3a1cefced0d58db42
Irina Bratkova <bratckova.i@yandex.ru>
2020-01-10 16:40:52 +0300

    Lesson: заголовок в норме

--- project/TestLessonPage.py
+++ project/TestLessonPage.py
@@ -6,10 +6,14 @@ from classes.lesson import Lessons
 
 class TestLessonPage(unittest.TestCase):
     def start_up(self):
-        self.url = 'http://localhost:8080/#/photolessons/10'
+        self.url = 'http://localhost:8080/#/photolessons/3'
         self.driver = webdriver.Chrome('./chromedriver')
         self.lesson = Lessons(self.driver, self.url)
-
-    def test_lesson(self):
-        self.start_up()
         self.lesson.open_url()
+
+    def test_title_is_ok(self):
+        self.start_up()
+        title = self.lesson.value_title()
+        self.assertEqual(title, 'Сказка')
+
+
