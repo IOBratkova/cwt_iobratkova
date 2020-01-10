c95b752db52837c78f7438d47965e9b094bdd877
Irina Bratkova <bratckova.i@yandex.ru>
2020-01-10 14:43:05 +0300

    update

--- project/program.py
+++ project/program.py
@@ -20,30 +20,12 @@ from classes.page import Page
 # ya_page.paste_to_element('text', 'кот')
 # text = ya_page.find_element_by_id('text')
 from selenium.webdriver.support.ui import WebDriverWait
-url = 'http://127.0.0.1:8080/#/signup'
 driver = webdriver.Chrome('./chromedriver')
-signinpage = SignInPage(driver, url)
-signinpage.open_url()
-signinpage.set_login('myNiceLogin')
-signinpage.set_email('thisisemail@em.com')
-signinpage.set_firstName('Василий')
-signinpage.set_lastName('Котиков')
-signinpage.set_textarea('Я люблю котиков. Мур!')
-signinpage.click_radio_button_master()
-signinpage.select_value_to_master(2)
-signinpage.set_autocomplete('Б')
-signinpage.click_on_autocomplete(3)
-signinpage.set_avatar('/home/irina/repo/cwt_iobratkova/bonnie.jpg')
-signinpage.set_password('cat')
-signinpage.set_ppassword('cat')
-signinpage.click_button_submit()
-turl =  driver.current_url
-signinpage.click_button_submit()
-WebDriverWait(signinpage.driver, 5).until(
-    lambda kek: signinpage.driver.current_url != turl
-)
-
-
-result = 'lol'
+url = 'http://localhost:8080/#/lessonlist'
+from classes.listlessons import LessonsList
+ll = LessonsList(driver, url)
+ll.open_url()
+ll.login_menu.click_dropdown_sign_in()
+ll.login_menu.click_login_button()
+result = ll.login_menu.click_login_button()
 print(result)
-
